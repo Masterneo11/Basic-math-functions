034f382062866f5a432a80e0eb068bd72525ad8d
Mathew Zamacona <mateoszamacona@icloud.com>
2024-02-28 14:31:57 -0700

    basic/math_functions

---
+++ math_fun.py
@@ -0,0 +1,28 @@
+
+
+def add(x,y) -> float:
+    return x + y
+
+def multiply(x, y) -> float:
+    return x * y
+
+def square(x) -> float:
+    return x * x
+
+def add_squares(x, y) -> float:
+    return x * x + y * y
+
+
+
+
+
+x = 5
+y = 2
+print("x = 5")
+print("y = 2")
+
+print(f"the add functoin is {add(x,y)} ")
+print(f" the multiply function is { multiply(x,y)} ")
+print(f" the square function is {square(x,)} ")
+print(f" the add squares is {add_squares(x,y)} ")
+
