125106634ff5931de4e8dd88f391a221e4105b0b
Mathew Zamacona <mateoszamacona@icloud.com>
2024-04-16 13:48:53 -0600

    another file

---
+++ math.py
@@ -0,0 +1,12 @@
+def add(x,y) -> float:
+    return float(x + y)
+
+def multiply(x, y) -> float:
+    return float(x * y)
+
+def square(x) -> float:
+    return float(x * x)
+
+def add_squares(x, y) -> float:
+    return float(x * x + y * y)
+
